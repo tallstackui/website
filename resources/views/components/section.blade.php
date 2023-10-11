ee299cb0385b25f5b7f05d4eb698b6db0f261155
AJ <alvaro.meireles@live.com>
2023-10-11 16:32:57 -0300

    texting

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -2,7 +2,7 @@
 
 <section {{ $attributes->merge(['class' => 'text-gray-600 mb-4']) }} @if ($id) id="{{ $id }}" @endif>
     @if ($title)
-        <h1 @class(['text-2xl tracking-tight text-slate-900 font-medium dark:text-white', 'mb-4' => !$description])>
+        <h1 @class(['text-2xl tracking-tight text-pink-600 font-medium dark:text-white', 'mb-4' => !$description])>
             {{ $title }}
         </h1>
     @endif
