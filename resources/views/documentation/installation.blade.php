67ed744c75ed2f16d5806cd60dcb43b4a75f5b92
AJ <alvaro.meireles@live.com>
2023-10-17 13:16:36 -0300

    introducing spa mode

--- resources/views/documentation/installation.blade.php
+++ resources/views/documentation/installation.blade.php
@@ -2,7 +2,7 @@
     <x-slot:title>
         Get Started
     </x-slot:title>
-    <x-section title="Requirements" id="requirements">
+    <x-section title="Requirements">
         <p>TallStackUi was created with a <b class="font-semibold text-pink-500 underline">focus on Livewire 3</b> so the usage requirements are:</p>
         <ul class="mt-4 ml-4 list-inside list-decimal space-y-2">
             <li>Laravel 10</li>
@@ -10,7 +10,7 @@
             <li>PHP 8.1</li>
         </ul>
     </x-section>
-    <x-section title="Installation" id="installation">
+    <x-section title="Installation">
         <p>1. Run the following command to include the TallStackUi as a composer dependency:</p>
         <x-code language="shell" :contents="$installation"/>
         <p>2. Add the TallStackUi setup above the <x-block>vite</x-block> tag in your layout:</p>
@@ -23,5 +23,11 @@
         <p>4. Build the assets and do a full cleanup in the Laravel:</p>
         <x-code language="shell" :contents="$build"/>
         <p>You're ready to create your first TallStackUi components.</p>
+    </x-section>
+    <x-section title="Livewire SPA Mode">
+        <p class="text-justify">
+            If you are using the new SPA mode in Livewire you must add the TallStackUi setup to the first page rendered
+            in your application and persist the TallStackUi setup in all other layouts that want to use TallStackUi.
+        </p>
     </x-section>
 </x-layout>
