3ee6d99f9695a80ece661c3642623c901656a317
AJ <alvaro.meireles@live.com>
2023-11-17 18:43:52 -0300

    formatting

--- resources/views/components/copy-to-clipboard.blade.php
+++ resources/views/components/copy-to-clipboard.blade.php
@@ -1,19 +1,19 @@
 @props(['contents' => null])
 
 <div x-data="{
-    copyText: @js($contents),
-    copyNotification: false,
-    copyToClipboard() {
-        navigator.clipboard.writeText(this.copyText);
-        this.copyNotification = true;
-        let that = this;
-        setTimeout(function() {
-            that.copyNotification = false;
-        }, 3000);
+    text: @js($contents),
+    notification: false,
+    copy() {
+        try {
+            navigator.clipboard.writeText(this.copyText);
+            this.notification = true;
+        } catch (err) { alert('Failed to copy to clipboard.'); }
+
+        setTimeout(() => this.notification = false, 3000);
     }
 }" class="relative z-20 flex items-center m-2">
-    <button x-on:click="copyToClipboard();">
-        <x-icon name="document" class="text-gray-500 h-5 w-5" x-show="!copyNotification" />
-        <x-icon name="document-check" class="h-5 w-5 text-pink-500" x-show="copyNotification" />
+    <button x-on:click="copy()">
+        <x-icon name="document" class="text-gray-500 h-5 w-5" x-show="!notification" />
+        <x-icon name="document-check" class="h-5 w-5 text-pink-500" x-show="notification" />
     </button>
 </div>
