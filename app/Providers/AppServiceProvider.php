2eb35a0db91e988d49894928da730197e329e869
AJ <alvaro.meireles@live.com>
2023-11-27 12:37:26 -0300

    adding https when testing by ngrok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        if ($this->app->isProduction()) {
+        if ($this->app->isProduction() || str_contains(config('app.url'), 'ngrok')) {
             URL::forceScheme('https');
         }
     }
