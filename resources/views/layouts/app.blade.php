e582117a5ac9b2e76a589752af566017a0a9b52c
Hítalo Ramon <hitaloramon@live.com>
2023-12-04 15:58:59 -0300

    button back to top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,11 +97,11 @@
     </div>
     <div class="fixed bottom-4 right-4 px-4 py-2">
         <x-button.circle icon="chevron-up" 
-                    color="pink"
-                    x-data="{ visible: false }"
-                    x-on:scroll.window="visible = window.scrollY > 100"
-                    X-on:click="window.scrollTo({ top: 0, behavior: 'smooth' })"
-                    x-show="visible"  />
+                         color="pink"
+                         x-data="{ visible: false }"
+                         x-on:scroll.window="visible = window.scrollY > 100"
+                         x-on:click="window.scrollTo({ top: 0, behavior: 'smooth' })"
+                         x-show="visible"  />
     </div>
     @livewireScripts
 </body>
