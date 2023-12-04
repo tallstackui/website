1bd4010984059441237dc021cfa6c90cd2c4148c
AJ <alvaro.meireles@live.com>
2023-12-04 12:45:39 -0300

    google analytics

---
+++ resources/views/components/google-analytics.blade.php
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-L13N82DRXC"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-L13N82DRXC');
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,6 +29,7 @@
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
+    <x-google-analytics />
 
     <tallstackui:script />
     @livewireStyles
