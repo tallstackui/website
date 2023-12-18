bd7ffc910e9f996764cc7b9642bfd9c937865265
AJ <60591772+devajmeireles@users.noreply.github.com>
2023-12-18 06:06:38 -0300

    Update top-bar.blade.php to reflects release link

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -3,7 +3,7 @@
             'text' => 'text-white'
         ]" until="2023-12-22">
     <b>Release 1.5.0 is here!</b> 🎉 <u>3 new components, debug mode, and more.</u>
-    <x-link :color="null" href="https://github.com/tallstackui/tallstackui/releases/tag/1.5.0"
+    <x-link :color="null" href="https://github.com/tallstackui/tallstackui/releases/tag/v1.5.0"
             icon="arrow-up-right"
             position="right"
             blank>
