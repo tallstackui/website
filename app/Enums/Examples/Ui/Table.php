93ff51424e7be90b92ad37a7b99413a89c774261
AJ <alvaro.meireles@live.com>
2023-12-27 14:34:45 -0300

    wip

--- app/Enums/Examples/Ui/Table.php
+++ app/Enums/Examples/Ui/Table.php
@@ -4,8 +4,258 @@ namespace App\Enums\Examples\Ui;
 
 class Table
 {
+    public const EXAMPLE_ARRAY = <<<'HTML'
+    @php
+        $headers = [
+            ['index' => 'id', 'label' => '#'],
+            ['index' => 'name', 'label' => 'Member'],
+        ];
+
+        $rows = [
+            ['id' => 1, 'name' => 'Taylor Otwell'],
+            ['id' => 2, 'name' => 'Nuno Maduro'],
+        ];
+    @endphp
+
+    <x-table :$headers :$rows />
+    HTML;
+
+    public const EXAMPLE_ELOQUENT = <<<'HTML'
+    <?php
+
+    // Example of Livewire through Laravel Volt.
+
+    use App\Models\User;
+    use Livewire\Volt\Component;
+
+    new class extends Component {
+
+        public function with(): array
+        {
+            return [
+                'headers' => [
+                    ['index' => 'id', 'label' => '#'],
+                    ['index' => 'name', 'label' => 'Member'],
+                ],
+                'rows' => User::all(),
+            ];
+        }
+    }; ?>
+
+    <div>
+        <x-table :$headers :$rows />
+    </div>
+    HTML;
+
     public const BASIC = <<<'HTML'
-    //
+    <?php
+
+    use App\Models\User;
+    use Livewire\Volt\Component;
+
+    new class extends Component {
+
+        public function with(): array
+        {
+            return [
+                'headers' => [
+                    ['index' => 'id', 'label' => '#'],
+                    ['index' => 'name', 'label' => 'Member'],
+                ],
+                'rows' => User::all(),
+            ];
+        }
+    }; ?>
+
+    <div>
+        <x-table :$headers :$rows />
+    </div>
+    HTML;
+
+    public const WITHOUT_HEADER = <<<'HTML'
+    <?php
+
+    use App\Models\User;
+    use Livewire\Volt\Component;
+
+    new class extends Component {
+
+        public function with(): array
+        {
+            return [
+                'headers' => [
+                    ['index' => 'id', 'label' => '#'],
+                    ['index' => 'name', 'label' => 'Member'],
+                ],
+                'rows' => User::all(),
+            ];
+        }
+    }; ?>
+
+    <div>
+        <x-table :$headers :$rows headerless />
+    </div>
+    HTML;
+
+    public const STRIPED = <<<'HTML'
+    <?php
+
+    use App\Models\User;
+    use Livewire\Volt\Component;
+
+    new class extends Component {
+
+        public function with(): array
+        {
+            return [
+                'headers' => [
+                    ['index' => 'id', 'label' => '#'],
+                    ['index' => 'name', 'label' => 'Member'],
+                ],
+                'rows' => User::all(),
+            ];
+        }
+    }; ?>
+
+    <div>
+        <x-table :$headers :$rows striped />
+    </div>
+    HTML;
+
+    public const FILTERS = <<<'HTML'
+    <?php
+
+    use App\Models\User;
+    use Livewire\Volt\Component;
+    use Illuminate\Database\Eloquent\Builder;
+
+    new class extends Component {
+
+        public int $quantity = 10; // [tl! highlight]
+
+        public ?string $search = null; // [tl! highlight]
+
+        public function with(): array
+        {
+            return [
+                'headers' => [
+                    ['index' => 'id', 'label' => '#'],
+                    ['index' => 'name', 'label' => 'Member'],
+                ],
+                'rows' => User::query()
+                    ->when($this->search, function (Builder $query) { // [tl! highlight:3]
+                        return $query->where('name', 'like', "%{$this->search}%");
+                    })
+                    ->paginate($this->quantity)
+                    ->withQueryString()
+            ];
+        }
+    }; ?>
+
+    <div>
+        <x-table :$headers :$rows filter />
+
+        <!-- You can control the quantities of the quantity selector -->
+        <x-table :$headers :$rows filter :quantity="[2,5,10]" />
+
+        <!-- You can specify different properties for the filter -->
+        <x-table :$headers :$rows filter :filters="['quantity' => 'foo', 'search' => 'bar']" />
+    </div>
+    HTML;
+
+    public const LOADING = <<<'HTML'
+    <?php
+
+    use App\Models\User;
+    use Livewire\Volt\Component;
+    use Illuminate\Database\Eloquent\Builder;
+
+    new class extends Component {
+
+        public int $quantity = 10;
+
+        public ?string $search = null;
+
+        public function with(): array
+        {
+            return [
+                'headers' => [
+                    ['index' => 'id', 'label' => '#'],
+                    ['index' => 'name', 'label' => 'Member'],
+                ],
+                'rows' => User::query()
+                    ->when($this->search, function (Builder $query) {
+                        return $query->where('name', 'like', "%{$this->search}%");
+                    })
+                    ->paginate($this->quantity)
+                    ->withQueryString()
+            ];
+        }
+    }; ?>
+
+    <div>
+        <x-table :$headers :$rows filter loading />
+    </div>
+    HTML;
+
+    public const SORT = <<<'HTML'
+    <?php
+
+    use Livewire\Volt\Component;
+    use App\Models\User;
+
+    new class extends Component {
+
+        public array $sort = [ // [tl! highlight:3]
+            'column' => 'id',
+            'direction' => 'desc',
+        ];
+
+        public function with(): array
+        {
+            return [
+                'headers' => [
+                    ['index' => 'id', 'label' => '#'],
+                    ['index' => 'name', 'label' => 'Member'],
+                ],
+                'rows' => User::query()
+                    ->orderBy(...array_values($this->sort)) // [tl! highlight]
+                    ->paginate(10)
+                    ->withQueryString()
+            ];
+        }
+    }; ?>
+
+    <div>
+        <x-table :$headers :$rows :$sort />
+    </div>
+    HTML;
+
+    public const PAGINATE = <<<'HTML'
+    <?php
+
+    use Livewire\Volt\Component;
+    use App\Models\User;
+
+    new class extends Component {
+
+        public function with(): array
+        {
+            return [
+                'headers' => [
+                    ['index' => 'id', 'label' => '#'],
+                    ['index' => 'name', 'label' => 'Member'],
+                ],
+                'rows' => User::query()
+                    ->paginate(10)
+                    ->withQueryString()
+            ];
+        }
+    }; ?>
+
+    <div>
+        <x-table :$headers :$rows paginate />
+    </div>
     HTML;
 
     public const PERSONALIZATION = <<<'HTML'
--- resources/views/documentation/ui/table.blade.php
+++ resources/views/documentation/ui/table.blade.php
@@ -13,33 +13,47 @@
         version 1.6.0</u> of TallStackUI you can take advantage of the simple table component,
         but with all the basic features necessary for a table to work through Livewire components.
     </x-section>
+    <x-section title="Data for Table" :copy="false">
+        You have two ways to provide data to create a table: <b>1)</b> Simple data through an array
+        or <b>2)</b> Data that comes from the database, using <a href="https://laravel.com/docs/10.x/pagination#paginating-eloquent-results" class="underline" target="_blank">
+        Laravel Eloquent Pagination.</a> The main difference between the two ways is that
+        when choosing to create a <u>table with an array of data, features such as filtering,
+        sorting and pagination will be more difficult to implement.</u>
+        <p class="mt-4"><b>1)</b> Table using array</p>
+        <x-code language="blade" :contents="$exampleArray" :copy="false" />
+        <p class="mt-4"><b>2)</b> Table using Laravel Eloquent</p>
+        <x-code language="blade" :contents="$exampleEloquent" :copy="false" />
+    </x-section>
+    <x-warning class="mb-4">
+        All examples below use Livewire components through Laravel Volt.
+    </x-warning>
     <x-section title="Basic Usage">
         <x-preview language="blade" :contents="$basic" :background="false">
             <livewire:documentation.ui.table :mode="1" />
         </x-preview>
     </x-section>
     <x-section title="Without Header">
-        <x-preview language="blade" :contents="$basic" :background="false">
+        <x-preview language="blade" :contents="$withoutHeader" :background="false">
             <livewire:documentation.ui.table :mode="2" />
         </x-preview>
     </x-section>
     <x-section title="Striped Style">
-        <x-preview language="blade" :contents="$basic" :background="false">
+        <x-preview language="blade" :contents="$striped" :background="false">
             <livewire:documentation.ui.table :mode="3" />
         </x-preview>
     </x-section>
     <x-section title="Filters">
-        <x-preview language="blade" :contents="$basic" :background="false">
+        <x-preview language="blade" :contents="$filters" :background="false">
             <livewire:documentation.ui.table :mode="4" />
         </x-preview>
     </x-section>
     <x-section title="Loading Effect" description="An option to display a loading effect when interacts with the table.">
-        <x-preview language="blade" :contents="$basic" :background="false">
+        <x-preview language="blade" :contents="$loading" :background="false">
             <livewire:documentation.ui.table :mode="5" />
         </x-preview>
     </x-section>
-    <x-section title="Sort" description="Sort feature when clicking on the header names of the table.">
-        <x-preview language="blade" :contents="$basic" :background="false">
+    <x-section title="Sorting" description="Sorting when clicking on the header names of the table.">
+        <x-preview language="blade" :contents="$sort" :background="false">
             <livewire:documentation.ui.table :mode="6" />
         </x-preview>
     </x-section>
