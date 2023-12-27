d7759bb6843b573c2de1c96a844c845d700c2c27
AJ <alvaro.meireles@live.com>
2023-12-27 15:47:56 -0300

    updating banner text

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -4,6 +4,6 @@
         ]" until="2023-12-31">
     <b>Release 1.6.0 is here!</b> 🎉
     <a href="{{ route('documentation.ui.table') }}" class="underline">
-        Say hello to the Table component.
+        Say hello to our new component: Table.
     </a>
 </x-banner>
