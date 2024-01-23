1fa5066c3b985f0edaecdeef2bb6c228998ddfcb
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-01-23 11:21:38 -0300

    command to delete tmp folder

---
+++ app/Console/Commands/DeleteLivewireTemporaryUpload.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class DeleteLivewireTemporaryUpload extends Command
+{
+    protected $signature = 'livewire:delete-temporary-upload';
+
+    protected $description = 'Delete the temporary upload folder of Livewire';
+
+    public function handle(): void
+    {
+        Storage::deleteDirectory('livewire-tmp');
+
+        if (app()->isProduction()) {
+            return;
+        }
+
+        $this->newLine();
+
+        $this->info('The temporary upload folder of Livewire has been deleted.');
+    }
+}
