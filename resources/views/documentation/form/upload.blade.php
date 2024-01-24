9b6be6b93b4ff78fe8d1e86b1ad35366a5a1ff68
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-01-24 00:33:11 -0300

    wip

--- resources/views/documentation/form/upload.blade.php
+++ resources/views/documentation/form/upload.blade.php
@@ -17,7 +17,7 @@
         file image. To disable the preview, simply pass the <b>:preview="false"</b> parameter
         to the component.
         <p class="mt-4 text-xs font-semibold underline">
-            All files uploaded as part of the test on this page are deleted each hour.
+            All files uploaded as part of tests on this page are deleted each hour.
         </p>
     </x-warning>
     <x-section title="Basic Usage">
