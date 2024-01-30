29be216e992cd2e1e836eb974f828b099a98d7da
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-01-30 11:19:07 -0300

    password events & top-bar activation

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,8 +1,5 @@
-<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" until="2024-01-26">
-    <a href="https://github.com/tallstackui/tallstackui/releases/tag/v1.13.0" target="_blank">
-        <b class="underline">Release 1.13.0 is here!</b> 🎉 Say hello to the new components:
-        <a href="{{ route('documentation.form.upload') }}" class="underline">Form Upload</a>
-        and
-        <a href="{{ route('documentation.ui.reaction') }}" class="underline">Reactions</a>
+<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" until="2024-02-02">
+    <a href="https://github.com/tallstackui/tallstackui/releases/tag/v1.14.0" target="_blank">
+        <b class="underline">Version 1.14.0 released!</b> 🎉 Introducing support for JS events across multiple components.
     </a>
 </x-banner>
