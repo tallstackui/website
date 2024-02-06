58c083a68cc85c90ba40938ab8bde2d1d0593e27
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-02-05 23:50:39 -0300

    top bar about summer release

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,5 +1,3 @@
-<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" until="2024-02-02">
-    <a href="https://github.com/tallstackui/tallstackui/releases/tag/v1.14.0" target="_blank">
-        <b class="underline">Version 1.14.0 released!</b> 🎉 Introducing support for JS events across multiple components.
-    </a>
+<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" until="2024-02-19">
+    The <b class="underline">summer release</b> is coming 🏄🏻 A new release with lots of new features.
 </x-banner>
