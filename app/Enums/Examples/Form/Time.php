5412cae98f389bc23d39368ff0781389feb68ae1
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-03-04 02:02:15 -0300

    wip

--- app/Enums/Examples/Form/Time.php
+++ app/Enums/Examples/Form/Time.php
@@ -8,6 +8,52 @@ class Time
     <x-time />
     HTML;
 
+    public const LABEL_HINT = <<<'HTML'
+    <x-time label="Time" hint="Select the hour" />
+    HTML;
+
+    public const FORMATS = <<<'HTML'
+    <!-- 12-hour format -->
+    <x-time />
+    
+    <!-- 24-hour format -->
+    <x-time format="24" />
+    HTML;
+
+    public const MIN_MAX = <<<'HTML'
+    <!-- Hours -->
+    <x-time :min-hour="5" :max-hour="10" />
+    
+    <!-- Minutes -->
+    <x-time :min-minute="30" :max-minute="45" />
+    HTML;
+
+    public const HELPER = <<<'HTML'
+    <x-time helper />
+    HTML;
+
+    public const STEP = <<<'HTML'
+    <x-time :step-hour="3" :step-minute="15" />
+    HTML;
+
+    public const FOOTER_SLOT = <<<'HTML'
+    <x-time>
+        <x-slot:footer>
+            Footer Slot
+        </x-slot:footer>
+    </x-time>
+    HTML;
+
+    public const EVENTS = <<<'HTML'
+    <x-time x-on:hour="alert(`Hour Selected: ${$event.detail.hour}`)"
+            x-on:minute="alert(`Minute Selected: ${$event.detail.minute}`)"
+            x-on:interval="alert(`Interval Changed: ${$event.detail.interval}`)"/>
+    HTML;
+
+    public const INVALIDATE = <<<'HTML'
+    <x-time invalidate />
+    HTML;
+
     public const PERSONALIZATION = <<<'HTML'
     TallStackUi::personalize()
         ->form('time')
--- resources/views/documentation/form/time.blade.php
+++ resources/views/documentation/form/time.blade.php
@@ -19,38 +19,38 @@
         </x-preview>
     </x-section>
     <x-section title="Label & Hint">
-        <x-preview language="blade" :contents="$basic">
+        <x-preview language="blade" :contents="$labelHint">
             <x-time label="Time" hint="Select the hour" />
         </x-preview>
     </x-section>
     <x-section title="Available Formats">
-        <x-preview language="blade" :contents="$basic">
+        <x-preview language="blade" :contents="$formats">
             <div class="space-y-2">
                 <x-time :value="$current" />
                 <x-time :value="$current" format="24" />
             </div>
         </x-preview>
     </x-section>
-    <x-section title="Min & Max Hour and Minutes">
-        <x-preview language="blade" :contents="$basic">
+    <x-section title="Min & Max Hours and Minutes">
+        <x-preview language="blade" :contents="$minMax">
             <div class="space-y-2">
                 <x-time label="Min Hour: 5, Max Hour: 10" :min-hour="5" :max-hour="10" />
                 <x-time label="Min Minute: 30, Max Minute: 45" :min-minute="30" :max-minute="45" />
             </div>
         </x-preview>
     </x-section>
-    <x-section title="Helper">
-        <x-preview language="blade" :contents="$basic">
+    <x-section title="Helper" description="An option to easily select the current time.">
+        <x-preview language="blade" :contents="$helper">
             <x-time format="24" helper />
         </x-preview>
     </x-section>
     <x-section title="Step Hour & Minute">
-        <x-preview language="blade" :contents="$basic">
+        <x-preview language="blade" :contents="$step">
             <x-time step-hour="3" step-minute="15" />
         </x-preview>
     </x-section>
     <x-section title="Footer Slot">
-        <x-preview language="blade" :contents="$basic">
+        <x-preview language="blade" :contents="$footerSlot">
             <x-time>
                 <x-slot:footer>
                     Footer Slot
@@ -59,14 +59,14 @@
         </x-preview>
     </x-section>
     <x-section title="Events">
-        <x-preview language="blade" :contents="$basic">
+        <x-preview language="blade" :contents="$events">
             <x-time x-on:hour="alert(`Hour Selected: ${$event.detail.hour}`)"
                     x-on:minute="alert(`Minute Selected: ${$event.detail.minute}`)"
                     x-on:interval="alert(`Interval Changed: ${$event.detail.interval}`)"/>
         </x-preview>
     </x-section>
     <x-section title="Invalidate" description="An option to not show validation error message.">
-        <x-preview language="blade" :contents="$basic">
+        <x-preview language="blade" :contents="$invalidate">
             <x-time invalidate />
         </x-preview>
     </x-section>
