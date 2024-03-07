99ade260525ec38ef0643388dc52a826831111b7
AJ <60591772+devajmeireles@users.noreply.github.com>
2024-03-07 00:22:50 -0300

    wip

--- resources/views/livewire/prefix-selector.blade.php
+++ resources/views/livewire/prefix-selector.blade.php
@@ -3,15 +3,24 @@
 use function Livewire\Volt\{state, rules, mount};
 use Illuminate\Support\Facades\Cookie;
 
-state(modal: false, prefix: null);
-rules(['prefix' => ['required', 'max:5']]);
+state(prefix: null);
+rules(['prefix' => ['nullable', 'max:10']]);
 
 mount(fn () => $this->prefix = Cookie::get('prefix'));
 
 $save = function () {
-    $this->validate();
+    $this->withValidator(function (\Illuminate\Validation\Validator $validator) {
+        $validator->after(function ($validator) {
+            if ($this->prefix && !preg_match('/^[A-Za-z-]+$/', $this->prefix)) {
+                return $validator->errors()->add('prefix', 'The prefix must contain only letters and dashes.');
+            }
+        });
+    })->validate();
 
-    Cookie::queue('prefix', $this->prefix, 60 * 24 * 365);
+    (match (filled($this->prefix)) {
+        true => fn () =>Cookie::queue(Cookie::forever('prefix', $this->prefix)),
+        default => fn () => Cookie::queue(Cookie::forget('prefix')),
+    })();
 
     $this->js(<<<JS
         location.reload();
@@ -20,20 +29,30 @@ $save = function () {
 
 ?>
 
-<div>
-    <x-button color="pink" class="w-full uppercase" wire:click="$toggle('modal')" xs>
-        {{ __('Configure Prefix') }}
-    </x-button>
-    <x-modal :title="__('Configure Prefix')" center wire>
-        <x-input label="Prefix"
-                 hint="Insert the prefix you are using in your project."
-                 wire:model="prefix"
-                 required
-                 max-length="5"/>
-        <x-slot:footer>
-            <x-button wire:click="save" color="green">
+<div class="ml-0.5">
+    <x-modal id="prefix-modal"
+             :title="__('CONFIGURE PREFIX')"
+             center
+             blur
+             persistent>
+        <p class="mb-2">
+            The documentation code examples are displayed without the <a href="{{ route('documentation.installation').'#' }}" class="underline">component prefix.</a>
+            In this form, you can configure the prefix you use so that all code examples
+            are displayed using the prefix, which makes it easier for you to copy and paste
+            the code without having to worry about manually defining the prefix in the code.
+        </p>
+        <form id="prefix" wire:submit="save">
+            <x-input label="Prefix"
+                     id="prefix-input"
+                     x-trap="show"
+                     hint="Leave it empty and press save to remove the prefix."
+                     wire:model="prefix"
+                     max-length="10"/>
+        </form>
+        <div class="mt-4">
+            <x-button class="w-full" form="prefix" type="submit" color="pink" sm>
                 SAVE
             </x-button>
-        </x-slot:footer>
+        </div>
     </x-modal>
 </div>
