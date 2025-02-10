db8df4080a291974d4206dc56511ceed17dcbfeb
AJ Meireles <alvaro.meireles@live.com>
2025-02-10 01:39:22 -0300

    wip

--- resources/views/documentation/v2/helpers/env-bar.blade.php
+++ resources/views/documentation/v2/helpers/env-bar.blade.php
@@ -1,3 +1,15 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-</div>
+@php
+    foreach (apply_prefix($__data) as $key => $value) $$key = $value;
+@endphp
+
+<x-layout :$content>
+    <x-slot:title>
+        Env Bar
+    </x-slot:title>
+    <x-slot:description>
+        A fixed bar utility at the top of the page.
+    </x-slot:description>
+    <x-section title="Concept" disable-copy>
+        //
+    </x-section>
+</x-layout>
