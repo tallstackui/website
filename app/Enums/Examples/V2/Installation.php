c36ff35d37176d63c5b6c05fce1adb58e0a6afd9
AJ Meireles <alvaro.meireles@live.com>
2025-02-17 21:06:36 -0300

    wip

--- app/Enums/Examples/V2/Installation.php
+++ app/Enums/Examples/V2/Installation.php
@@ -5,7 +5,7 @@ namespace App\Enums\Examples\V2;
 class Installation
 {
     public const INSTALLATION = <<<'HTML'
-    composer require tallstackui/tallstackui:^v2.0.0
+    composer require tallstackui/tallstackui:^2.0.0
     HTML;
 
     public const LAYOUT = <<<'HTML'
