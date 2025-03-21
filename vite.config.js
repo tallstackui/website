45c4429d2daef09dc937d072ddfc6b0f4b5a71be
AJ Meireles <alvaro.meireles@live.com>
2025-03-20 22:44:43 -0300

    wip

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
@@ -8,7 +11,11 @@ export default defineConfig({
                 'resources/css/app.scss',
                 'resources/js/app.js',
             ],
-            refresh: true,
+            refresh: [`resources/views/**/*`],
         }),
+        tailwindcss(),
     ],
+    server: {
+        cors: true,
+    },
 });
