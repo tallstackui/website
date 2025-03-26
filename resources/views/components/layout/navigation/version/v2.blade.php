478563254f2ed7f6d056a391d7ef045bfb8b4828
AJ Meireles <alvaro.meireles@live.com>
2025-03-25 23:49:12 -0300

    wip

--- resources/views/components/layout/navigation/version/v2.blade.php
+++ resources/views/components/layout/navigation/version/v2.blade.php
@@ -121,7 +121,7 @@
                         <x-layout.navigation.link :href="route('documentation', ['v2', 'ui', 'modal'])" text="Modal" />
                     </li>
                     <li class="relative ml-4">
-                        <x-layout.navigation.link :href="route('documentation', ['v2', 'ui', 'layout'])" text="Layout" />
+                        <x-layout.navigation.link :href="route('documentation', ['v2', 'ui', 'layout'])" text="Layout" new />
                     </li>
                     <li class="relative ml-4">
                         <x-layout.navigation.link :href="route('documentation', ['v2', 'ui', 'link'])" text="Link" />
