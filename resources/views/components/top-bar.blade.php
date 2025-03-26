e7de988466bf788bdd68781525c4bbdaadb7e46c
AJ Meireles <alvaro.meireles@live.com>
2025-03-25 23:46:16 -0300

    wip

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,11 +1,9 @@
 @if ($version === 'v2')
-<x-banner :color="[
-                'background' => 'bg-pink-900',
-                'text' => 'text-white'
-          ]" :text="[
+<x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']" :text="[
             'V2 is now our main version. V1 is considered EoL!',
-            'V2 now support both TailwindCSS versions, v3 and v4!',
-            'Please, consider sponsor TallStackUI project to keep it alive!',
+            'Starting from v2.3.0, TallStackUI support both TailwindCSS versions: v3 and v4!',
+            'Do you like the TallStackUI? Consider sponsor the project!',
+            'There are still dozens of new things to be done in 2025 in TallStackUI v2. Stay tuned for every release!',
         ]" />
 @else
     <x-banner :color="['background' => 'bg-red-600', 'text' => 'text-white']">
