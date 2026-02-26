de4774258bae2eeca1708e47d7db7d7a52f70105
AJ Meireles <alvaro.meireles@live.com>
2026-02-26 19:00:34 -0300

    Add v3 redirect and reorder version redirects from newest to oldest

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/docs', '/docs/v3/installation');
 Route::redirect('/docs/contribution', '/docs/v3/contribution');
-Route::redirect('/docs/v2', '/docs/v2/installation');
 Route::redirect('/contribution', '/docs/v2/contribution');
-Route::redirect('/docs/v1', '/docs/v1/getting-started');
 Route::redirect('/summer-release', '/docs/v1/summer-release');
 Route::redirect('/upgrade', '/docs/v3/upgrade-guide');
 Route::redirect('/install', '/docs/v3/installation');
+Route::redirect('/docs/v3', '/docs/v3/installation');
+Route::redirect('/docs/v2', '/docs/v2/installation');
+Route::redirect('/docs/v1', '/docs/v1/getting-started');
 Route::redirect('/issue', 'https://github.com/tallstackui/tallstackui/issues/new?template=bug_report.yml')->name('issue');
 
 Route::get('/ai/{name}.md', function (string $name) {
