991591006bfe9106f1b69322146080d9585732d0
AJ Meireles <alvaro.meireles@live.com>
2026-04-25 14:59:08 -0300

    wip

--- resources/views/components/top-bar.blade.php
+++ resources/views/components/top-bar.blade.php
@@ -1,6 +1,6 @@
 @if ($version === 'v3')
     <x-banner :color="['background' => 'bg-pink-900', 'text' => 'text-white']">
-        <a href="{{ route('documentation', ['v3', 'upgrade-guide']) }}">🎉 Welcome to the new major version: v3. <b>Upgrade now!</b></a>
+        <p>Say hello to the v3.2.0! <b>Three new components added: Accordion, Calendar and Timeline.</b></p>
     </x-banner>
 @else
     <x-banner :color="['background' => 'bg-red-600', 'text' => 'text-white']">
