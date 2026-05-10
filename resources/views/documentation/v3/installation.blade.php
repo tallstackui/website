e97712f537e1661762bc94f41ed42addb6065486
AJ <60591772+devajmeireles@users.noreply.github.com>
2026-05-10 00:41:44 -0300

    Update installation.blade.php

--- resources/views/documentation/v3/installation.blade.php
+++ resources/views/documentation/v3/installation.blade.php
@@ -3,7 +3,7 @@
         Installation
     </x-slot:title>
     <x-section class="mt-4" title="Meet TallStackUI" disable-copy>
-        TallStackUI is <b class="text-pink-600">a powerful suite of 30+ ready-to-use components for TALL Stack applications.</b>
+        TallStackUI is <b class="text-pink-600">a powerful suite of 50+ ready-to-use components for TALL Stack applications.</b>
         TallStackUI is unlike anything you've ever seen, from its conception to construction and general
         availability. Beforehand we want to tell you a little about our motivation.
     </x-section>
